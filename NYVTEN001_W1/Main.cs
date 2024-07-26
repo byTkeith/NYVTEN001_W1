1189c8ad41870b1460b3dd1be10a8d76718d66bd
Tendai <youremail@example.com>
2024-07-26 18:24:19 +0200

    added the main class

---
+++ NYVTEN001_W1/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYVTEN001_W1
+{
+    public class MainForm : Form
+    {
+        sta
+    }
+    
+
+}
