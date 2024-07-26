7c665ee0339d8300ce1a9514357799a21cbe3024
Tendai <youremail@example.com>
2024-07-26 18:08:24 +0200

    the july constructor added

--- NYVTEN001_W1/July.cs
+++ NYVTEN001_W1/July.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 public class July
 {
     private List<Horse> horses;
 
-    private July()
+    private July()// default constructor
     {
         horses = new List<Horse>();
     }
