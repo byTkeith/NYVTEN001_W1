046a409defe3b53dd0b9d31240f3b9e34121f028
Tendai <youremail@example.com>
2024-07-26 17:42:48 +0200

    Add project files.

---
+++ NYVTEN001_W1/Horse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+public class Horse
+{//get and setter methods
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int height { get; set; }
+    public double[] Times { get; set; }
+
+    public Horse()
+    {
+        Times= new double[3];
+    }
+
+    public Horse(string name, int age, int height, double[] times)
+    {
+        Name = name;
+        Age = age;
+        this.height = height;
+        Times = times;
+    }
+    public double AverageTime()
+    {
+
+    return Times.Average(); 
+    }
+    public string HorseDetails()
+    {
+        return $"Name: {Name}\tAge: {Age}\tHeight: {height}\tAverage run: {AverageTime()}";
+    }
+}
---
+++ NYVTEN001_W1/July.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYVTEN001_W1
+{
+    internal class July
+    {
+    }
+}
