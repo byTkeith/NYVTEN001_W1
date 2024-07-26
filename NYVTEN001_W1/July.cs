cded9054dccfb0a94f274b288ab48c7dc327ef95
Tendai <youremail@example.com>
2024-07-26 17:54:31 +0200

    added the july class

--- NYVTEN001_W1/July.cs
+++ NYVTEN001_W1/July.cs
@@ -3,10 +3,4 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace NYVTEN001_W1
-{
-    internal class July
-    {
-    }
-}
+public class July { }
