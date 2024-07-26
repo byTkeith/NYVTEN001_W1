62219fb4ef2439806fa832e0544b63d8960ce47d
Tendai <youremail@example.com>
2024-07-26 17:55:27 +0200

    created a horses object

--- NYVTEN001_W1/July.cs
+++ NYVTEN001_W1/July.cs
@@ -3,4 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public class July { }
+public class July
+{
+    private List<Horse> horses;
+
+}
