ca56e84deff67409a8bff3494a268c5d07165481
Tendai <youremail@example.com>
2024-07-26 19:07:43 +0200

    added horse constructor

--- NYVTEN001_W1/Main.cs
+++ NYVTEN001_W1/Main.cs
@@ -56,6 +56,16 @@ namespace NYVTEN001_W1
         private void BtnAdd_Click(object sender, EventArgs e)
         {
             string name=txtName.Text;
+            int age= int.Parse(txtAge.Text);
+            int height= int.Parse(txtHeight.Text);
+            double[] times= new double[] {
+            double.Parse(txtTime1.Text),
+            double.Parse(txtTime2.Text),
+            double.Parse(txtTime3.Text)
+            };
+
+            Horse horse= new Horse(name, age, height, times);
+            july.AddHorse( horse );
         }
     }
     
